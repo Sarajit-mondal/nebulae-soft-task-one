547105407d7613dba90c6c62453e7825da82028e
Sarajit-mondal <bdsarajit499@gmail.com>
2024-11-18 20:13:22 +0600

    nav bar design completed

--- src/app/components/pages/home/NavBar.jsx
+++ src/app/components/pages/home/NavBar.jsx
@@ -1,9 +1,12 @@
 "use client";
+import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
-import { AiOutlineHome, AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { FaHouse } from "react-icons/fa6";
 
 const links = [
-  { name: "Home", href: "#", icon: <AiOutlineHome className="w-5 h-5" /> },
+  { name: "", href: "/", icon: <FaHouse className="w-5 h-5" /> },
   { name: "Work", href: "#" },
   { name: "Services", href: "#" },
   { name: "Products", href: "#" },
@@ -25,43 +28,60 @@ export default function NavBar() {
   };
 
   return (
-    <nav className="bg-gray-800 text-white">
+    <nav
+      className="bg-gray-800 text-white h-screen"
+      //   backgroundImage and linear-gradient
+      style={{
+        backgroundImage:
+          "linear-gradient(to left, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.7)), url('https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <div className="container mx-auto flex items-center justify-between px-4 py-3">
         {/* Logo Section */}
         <div className="flex items-center">
-          <img src="/logo.png" alt="Logo" className="h-8 w-8 mr-2" />
-          <span className="text-xl font-bold">LogoText</span>
+          <Link href="/">
+            <Image
+              height={60}
+              width={200}
+              alt="logo"
+              src="https://acodez.in/wp-content/themes/acodez-theme/images/logo.png"
+            />
+          </Link>
         </div>
 
         {/* Menu Icon */}
-        <div className="md:hidden">
+        <div className="lg:hidden z-20">
           <button onClick={toggleMenu}>
             {isOpen ? (
-              <AiOutlineClose className="w-6 h-6" />
+              <AiOutlineClose className="w-8 h-8 font-extrabold" />
             ) : (
-              <AiOutlineMenu className="w-6 h-6" />
+              <AiOutlineMenu className="w-8 h-8 font-extrabold" />
             )}
           </button>
         </div>
 
         {/* Menu Items */}
         <ul
-          className={`fixed md:static inset-0 flex flex-col md:flex-row items-center justify-center bg-gray-800 md:bg-transparent md:space-x-8 transform ${
-            isOpen ? "translate-x-0" : "-translate-x-full"
-          } md:translate-x-0 transition-transform duration-300 md:transition-none`}
+          className={`fixed  md:static lg:flex inset-0 flex flex-col md:flex-row items-center justify-center bg-gray-800 md:bg-transparent md:space-x-8 transform ${
+            isOpen ? "translate-y-0 md:hidden" : "-translate-y-full"
+          } md:translate-y-0 transition-transform duration-300 md:transition-none`}
         >
+          {/* page links */}
           {links.map((link) => (
             <li key={link.name} className="p-2 md:p-0">
-              <a
+              <Link
                 href={link.href}
-                className={`flex items-center space-x-1 hover:text-blue-500 ${
-                  activeLink === link.name ? "text-blue-500 font-semibold" : ""
+                className={`flex items-center font-medium space-x-1 hover:text-blue-500 hover:duration-500 relative before ${
+                  activeLink === link.name ? "text-blue-500" : ""
                 }`}
                 onClick={() => handleLinkClick(link.name)}
               >
                 {link.icon && link.icon}
                 <span>{link.name}</span>
-              </a>
+              </Link>
             </li>
           ))}
           <li className="p-2 md:p-0">
